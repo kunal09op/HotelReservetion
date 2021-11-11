008e73a677c469c7ba6dc4d66c5e076e88cfc241
Kunal Rajendra Patil <3931@xceedance.com>
2021-11-11 10:23:36 +0530

    model added 1

--- HotelReservetion/Models/Hotel.cs
+++ HotelReservetion/Models/Hotel.cs
@@ -8,10 +8,10 @@ namespace HotelReservetion.Models
     public class Hotel
     {
          public int HotelID { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int ZipCode { get; set; }
-        public long PhoneNumber { get; set; }
+        public string HotelCity { get; set; }
+        public string HotelState { get; set; }
+        public int HotelZipCode { get; set; }
+        public long HotelPhoneNumber { get; set; }
         public int EmployeeID { get; set; }
     }
     public class Employee
@@ -19,5 +19,46 @@ namespace HotelReservetion.Models
         public int EmployeeID { get; set; }
         public string EmployeeName { get; set; }
         public long EmployeePhoneNumber { get; set; }
+    }
+    public class Room
+    {
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public int NumberOfBeds { get; set; }
+        public int Charges { get; set; }
+
+    }
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerCity { get; set; }
+        public string CustomerState { get; set; }
+        public int CustomerZipCode { get; set; }
+        public long CustomerPhoneNumber { get; set; }
+
+    }
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public int HotelID { get; set; }
+        public int CustomerID { get; set; }
+        public int RoomNumber { get; set; }
+        public String CheckInDate { get; set; }
+        public String CheckOutDate { get; set; }
+    }
+    public class Ratings
+    {
+        public int CustomerID { get; set; }
+        public int Rating { get; set; }
+        public String Comment { get; set; }
+       
+    }
+    public class Billing
+    {
+        public int BillNumber { get; set; }
+        public int ReservationID { get; set; }
+
+        public int TotalCharges { get; set; }
     }
 }
